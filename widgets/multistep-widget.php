b458cf90ee3161b2012534204c62b42f29569a03
Dominic Gitau <dominicnjoroge1@gmail.com>
2024-02-09 17:40:47 -0500

    created new file for handling multistep addon

---
+++ widgets/multistep-widget.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Multistep addon using elementor
+ * 
+ * @category Elementor,_Addon,_Form
+ * @package  ElementorMultistepAddon
+ * @author   Dominic Gitau <dominicnjoroge2@gmail.com>
+ * @license  <GPL2 class="0"></GPL2>
+ * @link     https://github.com/dominic-g/multistep_elementor 
+ */
